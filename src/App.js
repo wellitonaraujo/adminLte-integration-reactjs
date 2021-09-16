b5bdef7c4cbe071cbe671f71487ea15ff824e6bd
wellitonaraujo <wellitonaraujodev@hotmail.com>
2021-09-16 20:10:11 -0300

    Integrando ao Reactjs

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Header from './components/Header';
+import Menu from './components/Menu';
+import Dashboard from './components/Dashboard';
+import Footer from './components/Footer';
+import './App.css';
+
+function App() {
+  return (
+    <div  class="wrapper">
+      <Header />
+      <Menu />
+      <Dashboard />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
